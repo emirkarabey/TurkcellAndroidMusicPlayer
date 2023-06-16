3feabcdbad891fd96386091fe58952fea14e77b4
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-06-16 14:42:00 +0300

    Create hilt

--- app/src/main/java/com/emirk/turkcellandroidmusicplayer/presentation/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/emirk/turkcellandroidmusicplayer/presentation/favorite/FavoriteViewModel.kt
@@ -3,8 +3,11 @@ package com.emirk.turkcellandroidmusicplayer.presentation.favorite
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class FavoriteViewModel : ViewModel() {
+@HiltViewModel
+class FavoriteViewModel @Inject constructor() : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
