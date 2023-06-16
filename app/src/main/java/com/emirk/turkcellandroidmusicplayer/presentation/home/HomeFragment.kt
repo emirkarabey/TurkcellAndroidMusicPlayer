0f9d35f51a1e494a98370f36a9eda3eea09a3aee
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-06-16 16:19:45 +0300

    Get music categories

--- app/src/main/java/com/emirk/turkcellandroidmusicplayer/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/emirk/turkcellandroidmusicplayer/presentation/home/HomeFragment.kt
@@ -1,41 +1,57 @@
 package com.emirk.turkcellandroidmusicplayer.presentation.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.emirk.turkcellandroidmusicplayer.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        viewModel.getMusicCategories()
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        collectEvent()
+    }
+
+    private fun collectEvent() = binding.apply {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { uiState ->
+                    if (uiState.isLoading) {
+                        Log.v("HomeFragment", "Ui state loading")
+                    } else {
+                        Log.v("HomeFragment",uiState.musicCategories?.get(3)?.baseTitle.toString())
+                    }
+                }
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
