1e6b04cb7a61d293531ea40fa8f90b04d119b007
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-06-18 14:29:05 +0300

    Update reposiory module

--- app/src/main/java/com/emirk/turkcellandroidmusicplayer/di/RepositoryModule.kt
+++ app/src/main/java/com/emirk/turkcellandroidmusicplayer/di/RepositoryModule.kt
@@ -1,6 +1,10 @@
 package com.emirk.turkcellandroidmusicplayer.di
 
-import com.emirk.turkcellandroidmusicplayer.data.remote.repository.MusicCategoriesRepositoryImpl
+import com.emirk.turkcellandroidmusicplayer.data.repository.FavoriteRepositoryImpl
+import com.emirk.turkcellandroidmusicplayer.data.repository.FirebaseRepositoryImpl
+import com.emirk.turkcellandroidmusicplayer.data.repository.MusicCategoriesRepositoryImpl
+import com.emirk.turkcellandroidmusicplayer.domain.repository.FavoriteRepository
+import com.emirk.turkcellandroidmusicplayer.domain.repository.FirebaseRepository
 import com.emirk.turkcellandroidmusicplayer.domain.repository.MusicCategoriesRepository
 import dagger.Binds
 import dagger.Module
@@ -15,4 +19,10 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun musicCategoriesRepository(MusicCategoriesRepositoryImpl: MusicCategoriesRepositoryImpl): MusicCategoriesRepository
+    @Binds
+    @Singleton
+    abstract fun firebaseRepository(FirebaseRepositoryImpl: FirebaseRepositoryImpl): FirebaseRepository
+    @Binds
+    @Singleton
+    abstract fun favoriteRepository(FavoriteRepositoryImpl: FavoriteRepositoryImpl): FavoriteRepository
 }
